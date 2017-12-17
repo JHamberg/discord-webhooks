d50e1eb67498c84b86db036d964add44bfa46a2b
Jonatan Hamberg <jonatan.hamberg@cs.helsinki.fi>
2017-12-17 19:01:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Small program for sending discord messages via webhooks API.
+
+## How to run
+Make sure you have [Node](https://nodejs.org/en/) installed, then run `node start` or deploy straight to Heroku.
---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require("express")
+var request = require("request")
+var bodyparser = require("body-parser")
+
+var app = express()
+app.use(bodyparser.urlencoded({extended: true}))
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html")
+})
+app.post("/post", (req, res) => {
+	res.redirect("/")
+	console.log(req.body.webhook)
+	console.log(req.body.message)
+	console.log(req.body.username)
+	console.log(req.body.avatar_url)
+	request({
+		method: "POST",
+		url: req.body.webhook,
+		json: {
+			"content": req.body.message,
+			"username": req.body.username,
+			"avatar_url": req.body.avatar_url,
+		}
+	})
+})
+
+app.listen(process.env.PORT || 80, () => {
+	console.log("Started server successfully")
+})
